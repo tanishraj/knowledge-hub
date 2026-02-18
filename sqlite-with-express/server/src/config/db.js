10cea2eb3f53308fe625b322ff2a3142f9f8c541
Tanish Raj <learn.tanishraj@gmail.com>
2026-02-18 07:06:15 +0400

    remove sql-lite

--- sqlite-with-express/server/src/config/db.js
+++
@@ -1,6 +0,0 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
-
-const sqlite = new Database("../server/sqlite/chinook.db");
-// const sqlite = new Database(process.env.DB_PATH || 'db.sqlite');
-export const db = drizzle(sqlite);
